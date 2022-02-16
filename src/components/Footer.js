ca28d4062639979407280c05e73cb112b5372f2e
cathydomini <cathydominioni@Cathys-MBP.attlocal.net>
2022-02-16 11:00:44 -0800

    move slider to home

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Auther: Cathy Chou</p>
+      <p><a href="mailto:cathydomini@yahoo.com">cathydomini@yahoo.com</a></p>
+    </footer>
+  );
+};
+
+export default Footer;
